d91d3d87b376dfa4c41368639efbf11935a10421
andreresende36 <andreresende36@gmail.com>
2022-10-31 17:24:44 -0300

    Resolução do requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+//   return ids.forEach((id) => {
+//     if (curr.id === id) {
+//       acc.push(curr);
+//       console.log(`Id: ${id}`);
+//       console.log(`Curr: ${curr}`);
+//       console.log(`Acc: ${acc}`);
+//       return acc;
+//     }
+//     console.log(`Id: ${id}`);
+//     console.log(`Curr: ${curr}`);
+//     console.log(`Acc: ${acc}`);
+//     return acc;
+//   });
+// }, []);
+
+function getSpeciesByIds(...ids) {
+  return data.species.reduce((acc, curr) => {
+    if (ids.includes(curr.id)) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
 }
+console.log(getSpeciesByIds(
+  '0938aa23-f153-4937-9f88-4858b24d6bce',
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+));
 
 module.exports = getSpeciesByIds;
