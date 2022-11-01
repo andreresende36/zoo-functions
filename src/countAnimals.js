d93f7cb8a3905680c34ef4342df08cd1bcbb375e
andreresende36 <andreresende36@gmail.com>
2022-11-01 15:43:13 -0300

    Resolução do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+function countAnimals(object) {
+  if (!object) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const specieToCount = species.find((specie) => specie.name === object.specie);
+  if (object.sex === undefined) {
+    return specieToCount.residents.length;
+  }
+  return specieToCount.residents.filter((resident) => resident.sex === object.sex).length;
 }
+
+// console.log(countAnimals());
+// console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
