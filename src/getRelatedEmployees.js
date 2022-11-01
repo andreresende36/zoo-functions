9feccff31dbe537e8dcccbe1f34e027b69965bd7
andreresende36 <andreresende36@gmail.com>
2022-11-01 14:26:02 -0300

    Alteração do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,18 +2,22 @@ const data = require('../data/zoo_data');
 
 const { employees } = data;
 
-function isManager(idM) {
-  const idManagers = employees.reduce((acc, curr) => {
-    const { id, firstName } = curr;
-    if (firstName === 'Burl' || firstName === 'Ola'
-      || firstName === 'Stephanie' || firstName === 'Emery') {
-      acc.push(id);
-      return acc;
-    }
-    return acc;
-  }, []);
-  const employeeToCheck = employees.find((employee) => employee.id === idM);
-  return idManagers.includes(employeeToCheck.id);
+// function isManager(idM) {
+//   const idManagers = employees.reduce((acc, curr) => {
+//     const { id, firstName } = curr;
+//     if (firstName === 'Burl' || firstName === 'Ola'
+//       || firstName === 'Stephanie' || firstName === 'Emery') {
+//       acc.push(id);
+//       return acc;
+//     }
+//     return acc;
+//   }, []);
+//   const employeeToCheck = employees.find((employee) => employee.id === idM);
+//   return idManagers.includes(employeeToCheck.id);
+// }
+
+function isManager(id) {
+  return employees.some(({ managers }) => managers.some((managerId) => managerId === id));
 }
 
 function getRelatedEmployees(managerId) {
@@ -28,9 +32,5 @@ function getRelatedEmployees(managerId) {
     return acc;
   }, []);
 }
-
-// console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
-console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
-// c1f50212-35a6-4ecd-8223-f835538526c2
 
 module.exports = { isManager, getRelatedEmployees };
