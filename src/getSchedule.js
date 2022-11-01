9cb1d25b5c42b0d9c50b88e2d07b6e3e93c24742
andreresende36 <andreresende36@gmail.com>
2022-11-01 19:40:20 -0300

    Resolução do requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const weekDays = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+const animals = data.species.map((specie) => specie.name);
+const validEntries = weekDays.concat(animals);
+const { species, hours } = data;
 
+function getAnimalAviability(day) {
+  const animalsFiltered = species.filter((animal) => animal.availability.includes(day));
+  return animalsFiltered.map((animal) => animal.name);
+}
+function getOfficeHourExhibition(acc, curr) {
+  if (curr !== 'Monday') {
+    acc[curr] = {
+      officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
+      exhibition: getAnimalAviability(curr),
+    };
+    return acc;
+  }
+  acc[curr] = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+  return acc;
+}
+function getSchedule(scheduleTarget) {
+  if (!scheduleTarget || !validEntries.includes(scheduleTarget)) {
+    return weekDays.reduce(getOfficeHourExhibition, {});
+  }
+
+  if (animals.includes(scheduleTarget)) {
+    return species.find((element) => element.name === scheduleTarget).availability;
+  }
+
+  return getOfficeHourExhibition({}, scheduleTarget);
+}
+// console.log(getSchedule());
+// console.log(getSchedule('lions'));
+// console.log(getSchedule('lions'));
+// console.log(getAnimalAviability('Tuesday'));
 module.exports = getSchedule;
