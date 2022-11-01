b338aef64eff8e0c7bbf3ffe0c3dcf3824aee2a0
andreresende36 <andreresende36@gmail.com>
2022-11-01 17:23:50 -0300

    Resolução do requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+
+function countEntrants(array) {
+  const countChild = array.filter((entrant) => entrant.age < 18).length;
+  const countAdult = array.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const countSenior = array.filter((entrant) => entrant.age >= 50).length;
+  return { child: countChild, adult: countAdult, senior: countSenior };
 }
 
-function calculateEntry(entrants) {
-  // seu código    aqui
+function calculateEntry(array) {
+  if (!array || !array.length) {
+    return 0;
+  }
+  const entrantsCount = countEntrants(array);
+  const { prices } = data;
+  const entriesChild = entrantsCount.child * prices.child;
+  const entriesAdult = entrantsCount.adult * prices.adult;
+  const entriesSenior = entrantsCount.senior * prices.senior;
+  return entriesChild + entriesAdult + entriesSenior;
 }
 
+// console.log(countEntrants(entrants));
+// console.log(calculateEntry(entrants));
 module.exports = { calculateEntry, countEntrants };
